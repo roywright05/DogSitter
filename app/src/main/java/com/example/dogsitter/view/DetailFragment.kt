ac300b7650923976833cb4ed823ca7bae16fbd93
Roy C Wright <roy.c.wright05@gmail.com>
2019-12-10 16:15:38 -0500

    Arguments and animations complete

---
+++ app/src/main/java/com/example/dogsitter/view/DetailFragment.kt
@@ -0,0 +1,42 @@
+package com.example.dogsitter.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.example.dogsitter.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+
+class DetailFragment : Fragment() {
+
+    private var dogUuid = 0
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+            textView.text = dogUuid.toString()
+        }
+
+        btnToList.setOnClickListener {
+
+            val action = DetailFragmentDirections.actionDetailFragmentToListFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/dogsitter/view/ListFragment.kt
@@ -0,0 +1,35 @@
+package com.example.dogsitter.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.Navigation
+import com.example.dogsitter.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnToDetail.setOnClickListener {
+
+            val action = ListFragmentDirections.actionListFragmentToDetailFragment()
+            action.dogUuid = 5
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+
+}
